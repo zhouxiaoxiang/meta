bfcb9d5c09b275d8388fc9060257bb0c7ac51a68
Anton Kulyk <kuliks.anton@gmail.com>
2021-12-21 12:30:45 +0200

    Add tab-key navigation for metadata editor (#19379)
    * Forward form field ref
    
    * Focus "display_name" when selecting a new field
    
    * Hide query editor in metadata mode
    
    This will remove it from the "tab-flow"
    
    * Pass tabIndex prop to form text inputs
    
    * Add basic tab flow for metadata editor
    
    * Allow passing more props to SelectButton
    
    * Allow passing props to AccordionList's search input
    
    * Use numbers for tab indexes
    
    * Fix SelectButton's props type
    
    * Add special type picker to tab flow
    
    * Fix initial field focusing
    
    * Add "tab" icon
    
    * Add basic TabHintToast component
    
    * Show tab hint toast in metadata editor
    
    * Export wrapped CustomFormField
    
    * Forward SelectButton ref
    
    * Add react-virtualized scrollToColumn prop to Table
    
    * Don't open SemanticTypePicker on focus
    
    * Open SemanticTypePicker on "Enter" key press
    
    * Improve tabbing experience
    
    * automatically scroll the InteractiveTable while tabbing through columns
    * focus the first column on hitting "Enter" on the last column
    
    * Remove failed experiment
    
    It was my last hope to combine both props, but no lucj
    
    * Animate sidebar when focused field changes
    
    * Minor rename
    
    * Fix mapped field picker component
    
    * Pass visibility callbacks to trigger elements
    
    * Handle focus for mapped field picker
    
    * Pass searchPlaceholder to Select
    
    * Pass triggerable's onClose prop to Select
    
    * Add onClose prop to DataSelector
    
    * Use Select from special type picker
    
    * Handle focus for MappedFieldPicker
    
    * Add :focus style for essential select inputs
    
    * Fix jump from last to first column on tab key

--- frontend/src/metabase/components/Input/Input.styled.jsx
+++ frontend/src/metabase/components/Input/Input.styled.jsx
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 import { color, darken } from "metabase/lib/colors";
+import { forwardRefToInnerRef } from "metabase/styled-components/utils";
 
 export const InputRoot = styled.div`
   display: inline-flex;
@@ -8,7 +9,7 @@ export const InputRoot = styled.div`
   width: ${props => (props.fullWidth ? "100%" : "")};
 `;
 
-export const InputField = styled.input`
+export const InputField = forwardRefToInnerRef(styled.input`
   font-family: inherit;
   font-weight: 700;
   font-size: 1rem;
@@ -41,7 +42,7 @@ export const InputField = styled.input`
     css`
       width: 100%;
     `}
-`;
+`);
 
 export const InputIconContainer = styled.div`
   display: flex;
