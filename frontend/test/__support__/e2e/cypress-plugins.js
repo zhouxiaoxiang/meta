62af00994d7ddd4eb0e0fab7af1740ba45e794d3
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-01-26 22:09:51 +0100

    Enable Cypress coverage

--- frontend/test/__support__/e2e/cypress-plugins.js
+++ frontend/test/__support__/e2e/cypress-plugins.js
@@ -36,6 +36,7 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   require("cypress-grep/src/plugin")(config);
+  require("@cypress/code-coverage/task")(on, config);
 
   /********************************************************************
    **                          WEBPACK                               **
