a6e19f05e89260196dfc20bdff13ddc27f89e13b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-02-16 17:07:19 +0100

    Tweak cypress runner to run `currents` dashboard

--- frontend/test/__runner__/cypress-runner-run-tests.js
+++ frontend/test/__runner__/cypress-runner-run-tests.js
@@ -1,3 +1,5 @@
+const { spawn } = require("child_process");
+
 const cypress = require("cypress");
 const arg = require("arg");
 
@@ -9,15 +11,16 @@ const args = arg(
   { permissive: true }, // Passes all other flags and args to the Cypress parser
 );
 
+const cliArgs = args._;
+
 const folder = args["--folder"];
 const isFolder = !!folder;
 
 const isOpenMode = args["--open"];
 const isCI = process.env["CI"];
+const isCurrents = process.env["CURRENTS_KEY"];
 
 const parseArguments = async () => {
-  const cliArgs = args._;
-
   // cypress.cli.parseArguments requires `cypress run` as the first two arguments
   if (cliArgs[0] !== "cypress") {
     cliArgs.unshift("cypress");
@@ -44,46 +47,69 @@ const getReporterConfig = isCI => {
 };
 
 const runCypress = async (baseUrl, exitFunction) => {
-  const defaultConfig = {
-    configFile: "frontend/test/__support__/e2e/cypress.json",
-    config: {
-      baseUrl,
-    },
-    spec: isFolder && getSourceFolder(folder),
-  };
+  if (isCurrents) {
+    await runCurrents(baseUrl);
+  } else {
+    const defaultConfig = {
+      configFile: "frontend/test/__support__/e2e/cypress.json",
+      config: {
+        baseUrl,
+      },
+      spec: isFolder && getSourceFolder(folder),
+    };
 
-  const reporterConfig = getReporterConfig(isCI);
+    const reporterConfig = getReporterConfig(isCI);
 
-  const userArgs = await parseArguments();
+    const userArgs = await parseArguments();
 
-  const finalConfig = Object.assign(
-    {},
-    defaultConfig,
-    reporterConfig,
-    userArgs,
-  );
+    const finalConfig = Object.assign(
+      {},
+      defaultConfig,
+      reporterConfig,
+      userArgs,
+    );
 
-  try {
-    const { status, message, totalFailed, failures } = isOpenMode
-      ? await cypress.open(finalConfig)
-      : await cypress.run(finalConfig);
+    try {
+      const { status, message, totalFailed, failures } = isOpenMode
+        ? await cypress.open(finalConfig)
+        : await cypress.run(finalConfig);
 
-    // At least one test failed
-    if (totalFailed > 0) {
+      // At least one test failed
+      if (totalFailed > 0) {
+        await exitFunction(1);
+      }
+
+      // Something went wrong and Cypress failed to even run tests
+      if (status === "failed" && failures) {
+        console.error(message);
+
+        await exitFunction(failures);
+      }
+    } catch (e) {
+      console.error("Failed to run Cypress!\n", e);
+
       await exitFunction(1);
     }
-
-    // Something went wrong and Cypress failed to even run tests
-    if (status === "failed" && failures) {
-      console.error(message);
-
-      await exitFunction(failures);
-    }
-  } catch (e) {
-    console.error("Failed to run Cypress!\n", e);
-
-    await exitFunction(1);
   }
 };
+
+const baseCurrentsArgs = baseUrl => [
+  "currents",
+  "run",
+  "--config-file",
+  "frontend/test/__support__/e2e/cypress.json",
+  "--config",
+  `baseUrl=${baseUrl}`,
+];
+
+async function runCurrents(baseUrl) {
+  const currentsArgs = baseCurrentsArgs(baseUrl).concat(cliArgs);
+
+  const currentsProcess = spawn("yarn", currentsArgs, { stdio: "inherit" });
+
+  return new Promise((resolve, reject) => {
+    currentsProcess.on("exit", resolve);
+  });
+}
 
 module.exports = runCypress;
